9273d604558de1c6205a2e5c02d46d3873553203
DiegoHinA <91913263+DiegoHinA@users.noreply.github.com>
2022-02-07 23:53:14 -0400

    Update deploy-new.sh

--- compose/deploy-new.sh
+++ compose/deploy-new.sh
@@ -7,8 +7,13 @@ EDITION=${3:-community}
 
 # Ensure these are created so Docker doesn't create them as root
 #mkdir -p ~/.composer ~/.ssh
-
-
+mkdir /opt/pdata
+mkdir /opt/pdata/composer
+mkdir /opt/pdata/appdata
+mkdir /opt/pdata/ssh
+mkdir /opt/pdata/sockdata
+mkdir /opt/pdata/ssldata
+chmod 777 -R /opt/pdata/
 # &&'s are used below otherwise onelinesetup script fails/errors after bin/download
 bin/download "${VERSION}" "${EDITION}" \
   && bin/setup "${DOMAIN}"
